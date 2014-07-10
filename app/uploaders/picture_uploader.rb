14b745fd245437ce5c7aabfecf82f206d0d681c2
Erin Brown <emberbrown@gmail.com>
2014-07-10 00:41:50 -0400

    made thumb version, formatted show page

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -32,9 +32,13 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
+  process :resize_to_fit => [300, 400]
+
+
+  version :thumb do
   process :resize_to_fit => [200, 300]
-  # end
+  end
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
