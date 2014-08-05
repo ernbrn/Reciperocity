e838b11b6595efa588918d76a8c3ae8c779e4f3f
ernbrn <emberbrown@gmail.com>
2014-08-04 23:27:52 -0400

    create ingredient controller

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,50 @@
+class IngredientsController < Application Controller
+  before_action :find_ingredient, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def show
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    @ingredient.save
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def update
+    @ingredient.update(ingredient_params)
+    redirect_to @ingredient, notice: 'Ingredient was sucessfully updated.'
+  end
+
+  def destroy
+    @ingredient.destroy
+    redirect_to ingredients_url, notice: 'Ingredient was sucessfully destroyed.'
+  end
+
+  private
+
+  def find_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :amount, :measure,:recipe_id)
+
+
+
+
+
+
+
+
+
+
+
+
+  end
+end
